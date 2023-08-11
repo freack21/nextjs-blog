fd9d4d02880af175607a168144fc378bb0ac0f77
freack21 <fixri2104@gmail.com>
2023-08-11 14:24:22 +0700

    feat[all]: nak di upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "freack21.github.io",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+};
